4fdc6d7ba3990e2afc2c26d8a51cd5a54e24b9e9
725jywu-code <725jywu@gmail.com>
2026-01-11 12:17:50 +0800

    PlaneGame 類別 + 遊戲資料初始化

--- plane game2.py
+++ plane game2.py
@@ -14,3 +14,20 @@ COLOR_MISS = "white"
 COLOR_BODY = "#5555FF"
 COLOR_HEAD = "#FF4444"
 COLOR_TEXT = "black"
+
+class PlaneGame:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("尋找機頭 - 隨機變體版")
+        
+        self.grid_data = [[None for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
+        self.buttons = [[None for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
+        self.planes = []
+        self.total_heads = 0
+        self.found_heads = 0
+        self.steps = 0
+        self.max_steps = None
+        self.game_over = False
+
+        self.bomb_available = 1
+        self.steps_per_bomb = 5
