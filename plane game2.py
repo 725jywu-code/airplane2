8d17816c82e9deeb46b9e7022fc43d0a021549f2
725jywu-code <725jywu@gmail.com>
2026-01-11 12:17:16 +0800

    匯入套件 + 基本設定

---
+++ plane game2.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import messagebox, simpledialog
+import random
+
+# =============================
+# 基本設定
+# =============================
+GRID_SIZE = 10
+CELL_SIZE = 40
+PREVIEW_CELL_SIZE = 20
+
+COLOR_DEFAULT = "#DDDDDD"
+COLOR_MISS = "white"
+COLOR_BODY = "#5555FF"
+COLOR_HEAD = "#FF4444"
+COLOR_TEXT = "black"
